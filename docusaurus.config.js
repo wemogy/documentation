cb42149a53227f7131ddf09e7f96fb9c364c3d43
Robin-Manuel Thiel <robin-manuel@thiel1.de>
2021-04-21 00:55:43 +0200

    Updtae config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -84,12 +84,12 @@ module.exports = {
 				docs: {
 					sidebarPath: require.resolve('./sidebars.js'),
 					// Please change this to your repo.
-					editUrl: 'https://github.com/wemogy/docs/edit/main/'
+					editUrl: 'https://github.com/wemogy/documentation/edit/main/'
 				},
 				blog: {
 					showReadingTime: true,
 					// Please change this to your repo.
-					editUrl: 'https://github.com/wemogy/docs/edit/main/blog/'
+					editUrl: 'https://github.com/wemogy/documentation/edit/main/blog/'
 				},
 				theme: {
 					customCss: require.resolve('./src/css/custom.css')
