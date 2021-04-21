d6c6c26139b220ef4dcf37be6b6d84cf6a53eaa0
Robin-Manuel Thiel <robin-manuel@thiel1.de>
2021-04-21 11:25:54 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ module.exports = {
 	onBrokenMarkdownLinks: 'warn',
 	favicon: 'img/favicon.ico',
 	organizationName: 'wemogy', // Usually your GitHub org/user name.
-	projectName: 'docs', // Usually your repo name.
+	projectName: 'documentation', // Usually your repo name.
 	themeConfig: {
 		navbar: {
 			title: 'wemogy Docs',
